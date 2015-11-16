785d307bc4592c6c3c6daac397f83fa5b07fa36a
Eric Crosson <esc@ericcrosson.com>
2015-11-16 17:32:53 -0600

    now a single-page app
    - last coords are used as suggested input

--- src/quarc_user_interface/src/index.py
+++ src/quarc_user_interface/src/index.py
@@ -11,13 +11,16 @@ import rospy
 from quarc_user_interface.msg import user_input
 
 
+class CoordinatesInvalidException(Exception):
+    pass
+
+
 class SimpleUserInterface(object):
 
 
     def __init__(self):
         self.site_header = env.get_template('site_header.html').render()
         self.site_end = env.get_template('site_end.html').render()
-        #TODO: HOW DOES THE NODE HANDLER ADVERTISE?
         self.publisher = rospy.Publisher('user_interface', user_input, queue_size=10)
         rospy.init_node('quarc_user_interface')
 
@@ -35,39 +38,61 @@ class SimpleUserInterface(object):
 
 
     def parse_coordinates(self, coordinates):
-        coords = []
-        for i in coordinates.strip("()").split(","):
-            try:
-                coords.append(float(i))
-            except ValueError:
-                coords.append(-1)
+        try:
+            coords = []
+            for i in coordinates.strip("()").split(","):
+                try:
+                    coords.append(float(i))
+                except ValueError:
+                    coords.append(-1)
+        except AttributeError:
+            raise CoordinatesInvalidException()
         return tuple(coords)
 
 
     @cherrypy.expose
-    def index(self):
+    def index(self, pick_coordinates = None, place_coordinates = None):
+        try:
+            pick  = self.parse_coordinates(pick_coordinates)
+            place  = self.parse_coordinates(place_coordinates)
+            path = "%s -> %s" % (pick, place)
+            cherrypy.session['raw'] = (pick, place)
+            cherrypy.session['pick'] = pick
+            cherrypy.session['place'] = place
+            cherrypy.session['path_string'] = path
+            self.publish()
+        except CoordinatesInvalidException:
+            # try, try again
+            pick_coordinates = '(x,y,z)'
+            place_coordinates = '(x,y,z)'
+
         index = env.get_template('index.html') \
-                   .render(action='parse_path', default_text='(x,y,z)',
-                           pick_text='Pick object from:', pick_name='pick_coordinates',
-                           place_text='Place object at:', place_name='place_coordinates',
+                   .render(action='index',
+                           default_pick_text=place_coordinates,
+                           default_place_text=pick_coordinates,
+                           pick_text='Pick object from:',
+                           pick_name='pick_coordinates',
+                           place_text='Place object at:',
+                           place_name='place_coordinates',
                            submit_button_text='Start')
         return self.siteify(index)
 
 
     @cherrypy.expose
-    def parse_path(self, pick_coordinates, place_coordinates):
-        pick  = self.parse_coordinates(pick_coordinates)
-        place  = self.parse_coordinates(place_coordinates)
-        path = "%s -> %s" % (pick, place)
-        cherrypy.session['raw'] = (pick, place)
-        cherrypy.session['pick'] = pick
-        cherrypy.session['place'] = place
-        cherrypy.session['path_string'] = path
-        page = env.get_template('parse_path.html') \
-                  .render(action='index', path=path,
-                          submit_button_text='Plan another path')
-        self.publish()
-        return self.siteify(page)
+    # @cherrypy.expose
+    # def parse_path(self, pick_coordinates, place_coordinates):
+    #     pick  = self.parse_coordinates(pick_coordinates)
+    #     place  = self.parse_coordinates(place_coordinates)
+    #     path = "%s -> %s" % (pick, place)
+    #     cherrypy.session['raw'] = (pick, place)
+    #     cherrypy.session['pick'] = pick
+    #     cherrypy.session['place'] = place
+    #     cherrypy.session['path_string'] = path
+    #     page = env.get_template('parse_path.html') \
+    #               .render(action='index', path=path,
+    #                       submit_button_text='Plan another path')
+    #     self.publish()
+    #     return self.siteify(page)
 
 if __name__ == '__main__':
     conf = {
